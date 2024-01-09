c31329fabeeadcb7fd47158e12e51204f748a68a
leadgfl13 <leadgfl13@gmail.com>
2024-01-09 13:12:46 -0500

    Add a button to form container to make it disappear

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 
 let addBook = document.getElementById('add')
+let uploadBook =document.getElementById('upBook')
 let formcontainer = document.getElementById('formcontainer')
 addBook.addEventListener('click',()=>{
     alert("I am clicked")
     formcontainer.style.display = "flex"
 
 })
+
+uploadBook.addEventListener('click',()=>{
+    formcontainer.style.display = 'none'
+})
+
