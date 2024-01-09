d8a261ca40776d01b8b6259028651d9c633e3e8a
leadgfl13 <leadgfl13@gmail.com>
2024-01-08 22:21:04 -0500

    Add formcontainer box, and when add book button is pressed it changes display from none to flex

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 
 let addBook = document.getElementById('add')
+let formcontainer = document.getElementById('formcontainer')
 addBook.addEventListener('click',()=>{
     alert("I am clicked")
+    formcontainer.style.display = "flex"
+
 })
