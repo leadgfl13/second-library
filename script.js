1ae75cffce3cb3f6632fc9c60d84dbc1225e689e
leadgfl13 <leadgfl13@gmail.com>
2024-01-10 10:31:26 -0500

    Add functions to event listeners to blur background and then return background to normal when form pops up and goes away

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-
-
+let main = document.getElementById('main')
 let addBook = document.getElementById('add')
 let uploadBook =document.getElementById('upBook')
 let formcontainer = document.getElementById('formcontainer')
 addBook.addEventListener('click',()=>{
     formcontainer.style.display = "flex"
+    main.style.filter = 'blur(10px)'
+    main.style.pointerEvents = 'none'
 
 })
 
 uploadBook.addEventListener('click',()=>{
     formcontainer.style.display = 'none'
+    main.style.filter = 'blur(0px)'
+    main.style.pointerEvents = 'auto'
 })
 
