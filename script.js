5a3677c54165bba860dae06d129b32d61570a41a
leadgfl13 <leadgfl13@gmail.com>
2024-01-11 21:08:00 -0500

    Add fuctions to take the author, title, and book pages and add them to the book card

--- script.js
+++ script.js
@@ -13,18 +13,32 @@ uploadBook.addEventListener('click',()=>{
     formcontainer.style.display = 'none'
     main.style.filter = 'blur(0px)'
     main.style.pointerEvents = 'auto'
+    addInfo()
     clear(fields)
 })
 
 let fields = ['bookTitle', 'bookAuthor','pages']
 // Can I put a for loop?
-function clear(poo){
-    for (let i = 0; i<poo.length; i++ in poo){
-        let clearthing = document.getElementById(fields[i])
+function clear(list){
+    for (let i = 0; i<list.length; i++){
+        let clearthing = document.getElementById(list[i])
         clearthing.value = ''
 
-    }
-    
 
+    }
 }
 
+//Need function to take values and put them in form
+
+function addInfo(){
+    let enterT = document.getElementById('enteredtitle')
+    let bookName = document.getElementById('bookTitle')
+    enterT.innerHTML = bookName.value
+    let enterA= document.getElementById('enteredauthor')
+    let bookAuthor = document.getElementById('bookAuthor')
+    enterA.innerHTML = bookAuthor.value
+    let enterP = document.getElementById('enteredpages')
+    let bookPages = document.getElementById('pages')
+    enterP.innerHTML =bookPages.value
+
+}
